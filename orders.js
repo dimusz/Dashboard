c892fa93732d0aa4935dc19ea1817dfe785615f6
dimusz <dina.lucian@yahoo.com>
2023-03-27 11:45:38 +0300

    dashboard

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "iPhone5",
+    productNumber: "123",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "iPhone6",
+    productNumber: "1234",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "iPhone7",
+    productNumber: "12345",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "iPhone8",
+    productNumber: "123456",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "iPhone9",
+    productNumber: "1234567",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
